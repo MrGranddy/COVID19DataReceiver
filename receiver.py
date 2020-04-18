d1aa45cf9cc0cbbe3e039ef2977bbc8e90eaf08d
MrGranddy <bugrayesilkaynak@gmail.com>
2020-04-18 20:13:38 +0300

    First version of data receiver
    Now one can get daily data for active cases, total cases, new deaths and total deaths for a country

--- receiver.py
+++ receiver.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+import datetime
+
+enumerate_months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
 
 class COVID19_Receiver:
     def __init__(self):
@@ -29,21 +33,56 @@ class COVID19_Receiver:
         # Hold country list for future requests
         self.country_list = country_list
 
-    def get_daily_deaths(self, country):
-        pass
-
-    def get_daily_cases(self, country):
+    def get_country_data(self, country):
         if country.lower() in self.country_list:
+            # Get country detalils page
             html_content = requests.get(self.main_url + self.country_list[country]).text
             soup = BeautifulSoup(html_content, "lxml")
+
+            # The data is kept in scripts as arrays
+            scripts = soup.find_all("script", {"type": "text/javascript"})
+
+            # Get related scripts
+            data_scripts = []
+            for script in scripts:
+                if "data: " in str(script):
+                    data_scripts.append(str(script))
+
+            # Extract data from the scripts
+            data = []
+            for script in data_scripts:
+                try:
+                    m = re.search(r"data: \[([0-9,]*)\]", script).group(1)
+                    data.append( [ int(x) for x in m.strip().split(",") ] )
+                except:
+                    pass
+
+            # We get the time labels from any of the data scripts
+            time_labels = re.search(r'xAxis: {(\s)*categories: \[([0-9A-Za-z\s",]*)\]',
+                         data_scripts[2]).group(2)
+            time_labels = time_labels.strip().split(",")
+            
+            # Convert labels into datetime
+            datetimes = []
+            for label in time_labels:
+                raw = label.replace('"', '')
+                raw = raw.lower()
+                month, day = raw.split()
+                datetimes.append( datetime.date(2020, enumerate_months.index(month)+1, int(day)) )
+
+            # Prepare data to return
+            data_dict = {}
+            data_dict["daily_total_cases"] = data[0]
+            data_dict["daily_active_cases"] = data[1]
+            data_dict["daily_total_deaths"] = data[2]
+            data_dict["daily_new_deaths"] = data[3]
+            data_dict["dates"] = datetimes
+
+
+            return data_dict
+
         else:
             raise "The country you are looking for does not exist."
 
-    def get_daily_new_cases(self, country):
-        pass
-
     def get_country_list(self):
         return sorted(list(self.country_list.keys()))
-
-rec = COVID19_Receiver()
-print(rec.get_country_list())
